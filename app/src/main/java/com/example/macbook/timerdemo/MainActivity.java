119714baecdea2a3456194ed05cabddefcdf48b5
Macbook <yzchen1994@hotmail.com>
2016-04-29 22:47:51 -0400

    Handler Runnable post

--- app/src/main/java/com/example/macbook/timerdemo/MainActivity.java
+++ app/src/main/java/com/example/macbook/timerdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.macbook.timerdemo;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Handler handler = new Handler();
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                //Code to be run every second.
+                Log.i("Runnable has run", " a second passed.");
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        handler.post(run);
     }
 }
